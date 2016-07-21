f19da4dcd59671ce0c14907b17a80208a67bf278
Arjay Angeles <aqangeles@gmail.com>
2016-07-21 15:51:14 +0800

    Fix no article found layout.

--- clean-blog/category/partials/no-articles.blade.php
+++ clean-blog/category/partials/no-articles.blade.php
@@ -1,3 +1,7 @@
 <section class="category-article">
-    <h3>{{trans('cms::categories.no-articles')}}</h3>
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+            <h2 class="post-title">{{trans('cms::categories.no-articles')}}</h2>
+        </div>
+    </div>
 </section>
